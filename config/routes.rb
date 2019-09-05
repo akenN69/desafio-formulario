1ee1e5ea5d014e418cc83d092b3dc4553130f844
Seba_virtual <rata_6952@hotmail.com>
2019-09-05 16:26:32 -0400

    add form

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'sales/new'
+  post 'sales/create', as:'sales'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
